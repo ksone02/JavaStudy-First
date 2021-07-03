03fe1d08062040668f7268e18418840c806e668b
ksone02 <eksovus@gmail.com>
2021-07-04 00:15:37 +0900

    package 사용법

--- src/javastudy/Car.java
+++ src/javastudy/Car.java
@@ -11,4 +11,17 @@ public class Car {
 		//내 필드 name에다가 지금 들어온 name을 넣어주라! 라는 의미
 		this.name = name;
 	}
+	
+	//생성자도 오버로딩 가능.
+	//기본 생성자 추가
+	public Car() {
+//		this.name = "이름업소용";
+//		this.number = 0; -> 아래와 중복, 비효율적
+		this("이름없음", 0); //아래에 있는 생성자 호출됨.
+	}
+	
+	public Car(String name, int number) {
+		this.name = name;
+		this.number = number;
+	}
 }
---
+++ src/javastudy/HelloExam.java
@@ -0,0 +1,13 @@
+package javastudy;
+
+import com.eightcruzjavastudy.Hello; // ctrl + shift + o
+// * -> 모든 클래스 사용할거야! 라는 의미
+
+public class HelloExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hello hello = new Hello(); //다른패키지에 있기때문에 안돼 ㅠ import 해줘야 해
+	}
+
+}
