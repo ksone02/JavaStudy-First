2f7afdb82633fcc8ddb9041fbc7821be35adf9f6
ksone02 <eksovus@gmail.com>
2021-07-08 03:39:50 +0900

    인터페이스 시작

--- src/javastudy/TV.java
+++ src/javastudy/TV.java
@@ -1,5 +1,12 @@
 package javastudy;
 
 public interface TV {
-
+	public int MIN_VOLUME=0;
+	public int MAX_VOLUME=100; //상수
+	
+	//class대신 interface
+	public void turnOn();
+	public void turnOff();
+	public void changeVolume(int volume);
+	public void changeChannel(int channel);
 }
